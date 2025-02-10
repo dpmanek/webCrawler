6ac9f3ab9731f95c7fcbc4e0380ef92f3ee19d69
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2025-02-09 23:21:14 -0500

    Implement bid data scraping and CSV export functionality; update environment variables and dependencies

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
-import { loginToCivcastUsa } from "./scripts/index.js";
+import { scrapeData } from "./scripts/index.js";
 
 // Load environment variables
 dotenv.config();
 
 // Runnnig test script to scrape data from the website
-await loginToCivcastUsa();
+await scrapeData();
--- scripts/testScript1.js
+++
@@ -1,7 +0,0 @@
-import puppeteer from "puppeteer";
-
-export async function loginToCivcastUsa() {
-  console.log("Logging into CivCast USA");
-  console.log("Opening browser...");
-  console.log(process.env.USERNAME, process.env.PASSWORD);
-}
